c680d377cd13666cb27b7dd4d3a75a792ded58e9
Steven Leib <leib3@uic.edu>
2017-04-26 20:54:17 -0500

    fixed some janky VHDL code (port mapping just one bit from a vector output, port mapping an expression instead of a signal)  so that modelsim can run RTL simulation.

--- ASMDIVDP.vhd
+++ ASMDIVDP.vhd
@@ -44,21 +44,26 @@ component divReg6 is
 	 signal BInv : std_logic_vector(4 downto 0);
 	 signal AddOut, r: std_logic_vector(5 downto 0);
 	 signal CntOut: std_logic_vector(3 downto 0);
+	 signal A : std_logic_vector(15 downto 0);
+	 signal b : std_logic_vector(5 downto 0);
 begin
    RReg : divReg6 port map(d=>RRegIn,
         reset=>RRes, load=>RLoad, clock=>clock, q=>R);
 	AReg : divShftReg16 port map(d=>ain, clock=>clock, load=>ALoad, 
-	                            shift=>AShift, serialIn=>'0', serialOut=>ASerialOut, q(15)=>RRegIn(0));
+	                            shift=>AShift, serialIn=>'0', serialOut=>ASerialOut, q=>A);
    QReg : divShftReg16 port map(d=>"0000000000000000",clock=>clock, load=>'0', shift=>QShift, 
 	                             serialIn=>Cout, q=>Q);
 	DivCntr: DivDnCnt port map(d=>"1111", count=>CntOut, load=>CntLoad, en=>CntEn, 
 	                           clock=>clock);
-	Adder: divAdd6 port map(a=>R, b=>('1' & BInv), cin=>'1', cout=>cout, sum=>AddOut);
-	BInv<= not Bin;
+	Adder: divAdd6 port map(a=>R, b=>b, cin=>'1', cout=>cout, sum=>AddOut);
 	with cout select 
 	   RRegIn(5 downto 1) <=
 		    AddOut(4 downto 0) when '1',
 			 R(4 downto 0) when others;
 	z<=NOT(CntOut(3) OR CntOut(2) OR CntOut(1) OR CntOut(0));
 	rout<=r(5 downto 1);
+	
+RRegIn(0)<=A(15); --completes shift from A to R
+BInv<= not Bin;
+b <= '1' & BInv;
 end components;
